18e1ca1ded3a6ea45787b08ad0d73f01cb3e5376
MaartenComhair <maarten.comhair@gmail.com>
2026-04-09 21:46:48 +0200

    Siebe cleanup

--- MyPygame/main.py/main.py
+++ MyPygame/main.py/main.py
@@ -5,65 +5,67 @@ def round_list (Float_lijst):
     return situatie_1_rounded
 
 
-
-def simulatie_hypothecaire_lening (lening, jaren, rente, start_periode, eind_periode, extra=0, maand=0):
-
-    duur = jaren * 12
-    rente_p = rente / 100
-    rente_m = rente_p / 12
-    mensualiteit = lening * (rente_m * (1 + rente_m)**duur) / ((1 + rente_m)**duur - 1)
-
-    totale_kost = 0
-    totale_kost_2 = 0
-    interest_kosten_max = 0
-    interest_kosten_2 = 0
-    maanden_max = 0
-    maanden_betaald = 0
-    kapitaal_kosten = 0
-    
+def bereken_totaal(maanden_totaal, lening_totaal, mensualiteit, rente_m, extra_m, maand):
     interest_lijst = []
     kapitaal_lijst = []
-
-
+    interest_kosten_max = 0
+    totale_kost = 0
     #for loop voor berekening gegevens bij lening met volledige duurloop.
-    for i in range(1, jaren*12 + 1):
+    for i in range(1, maanden_totaal + 1):
 
-        interest = lening * rente_m
-        kapitaal = mensualiteit - interest
-        maanden_max = jaren * 12 
-
-        lening = lening - kapitaal - extra
-        interest_kosten_max = interest_kosten_max + interest
-        totale_kost = totale_kost + interest + kapitaal + extra
+        interest_m = lening_totaal * rente_m
+        kapitaal_m = mensualiteit - interest_m
         
 
-        interest_lijst.append(interest)
-        kapitaal_lijst.append(kapitaal)
+        lening_totaal -= (kapitaal_m + extra_m)
+        interest_kosten_max += interest_m
+        totale_kost += interest_m + kapitaal_m + extra_m
+
+        interest_lijst.append(interest_m)
+        kapitaal_lijst.append(kapitaal_m)
 
         #print(interest)
         #print(kapitaal)
 
-        if i == maand:
-            print(F"Deze maand betaalt u {round(interest,1)} euro interest & u stort {round(kapitaal,1)} euro terug")
-   
-        if lening < 0:
+        if maand == i:
+            print(F"Deze maand betaalt u {round(interest_m,1)} euro interest & u stort {round(kapitaal_m,1)} euro terug")
+
+        if lening_totaal < 0:
             break
+
+    return interest_lijst, kapitaal_lijst, interest_kosten_max, totale_kost
+    
+def simulatie_hypothecaire_lening (lening_totaal, jaren, rente, start_periode, eind_periode, extra_m=0, maand=0):
+
+    totale_kost_periode = 0
+    interest_kosten_periode = 0
+    maanden_betaald = 0
+    kapitaal_kosten = 0
+
+    maanden_totaal = jaren * 12
+    rente_p = rente / 100
+    rente_m = rente_p / 12
+    mensualiteit = lening_totaal * (rente_m * (1 + rente_m)**maanden_totaal) / ((1 + rente_m)**maanden_totaal - 1)
+
+    interest_lijst, kapitaal_lijst, interest_kosten_max, totale_kost = \
+        bereken_totaal(maanden_totaal, lening_totaal, mensualiteit, rente_m, extra_m, maand)
 
     
     #for loop voor berekening interest bij lening met deelse duurloop.
+    for i in range(len(interest_lijst)):
+        if i >= start_periode * 12 and i < eind_periode * 12:
+            maanden_betaald += 1
+            interest_kosten_periode += interest_lijst[i]
+            kapitaal_kosten += kapitaal_lijst[i]
 
-    for j in range(len(interest_lijst)):
-        if j >= start_periode * 12 and j < eind_periode * 12:
-            maanden_betaald = maanden_betaald + 1
-            interest_kosten_2 = interest_kosten_2 + interest_lijst[j]
-            kapitaal_kosten = kapitaal_kosten + kapitaal_lijst[j]
+        extra_betaald = extra_m * maanden_betaald
+        totale_kost_periode = interest_kosten_periode + kapitaal_kosten + extra_betaald
 
-        extra_betaald = extra * maanden_betaald
-        totale_kost_2 = interest_kosten_2 + kapitaal_kosten + extra_betaald
+    return mensualiteit, totale_kost, interest_kosten_max, maanden_totaal, extra_betaald, totale_kost_periode, interest_kosten_periode, kapitaal_kosten, maanden_betaald
 
-    return mensualiteit, totale_kost, interest_kosten_max, maanden_max, extra_betaald, totale_kost_2, interest_kosten_2, kapitaal_kosten, maanden_betaald
-
-
+def print_lening_dict(lening_dict):
+    for key, value in lening_dict.items():
+        print(f"{key}: {value}")
 
 voorvoegsels = ['mensualiteit',
                 'maximale totale kost',
@@ -79,38 +81,35 @@ voorvoegsels = ['mensualiteit',
   
 situatie_1 = round_list(
     simulatie_hypothecaire_lening(
-        lening= 275000,
+        lening_totaal= 275000,
         jaren= 25,
-        rente= 3.0,
+        rente= 3,
         start_periode= 0,
         eind_periode= 10,
-        extra=0))
+        extra_m=0))
 
 #lijst --> dict + voorvoegsels
 situatie_1_dict = dict(zip(voorvoegsels,situatie_1))
 
 print("Lening 1: \n")
-for key, value in situatie_1_dict.items():
-    print(f"{key}: {value}")
+print_lening_dict(situatie_1_dict)
 
 
 situatie_2 = round_list(
     simulatie_hypothecaire_lening(
-        lening= 275000,
+        lening_totaal= 275000,
         jaren= 25,
         rente= 3.5,
         start_periode= 0,
         eind_periode= 10,
-        extra= 0))
+        extra_m= 0))
 
 #lijst --> dict + voorvoegsels
 situatie_2_dict = dict(zip(voorvoegsels,situatie_2))
 
 print()
 print("lening2: \n")
-
-for key, value in situatie_2_dict.items():
-    print(f"{key}: {value}")
+print_lening_dict(situatie_2_dict)
 
 
 
@@ -121,9 +120,3 @@ def hypothecaire_lening_vergelijken(situatie_1, situatie_2):
 
 vergelijking_interest = hypothecaire_lening_vergelijken(situatie_1_dict,situatie_2_dict)
 print(round(vergelijking_interest,1))
-
-
-
-
-
-
